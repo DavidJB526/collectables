ebef1a977c1739a85ce99ac259802c2235b52111
DavidJB526 <david.beaudoin@loop.colum.edu>
2018-11-12 19:00:51 -0600

    Disable Carrot Sprite and Box Collider

--- Assets/Scripts/Collectable.cs
+++ Assets/Scripts/Collectable.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class Collectable : MonoBehaviour {
 
     private AudioSource audioSource;
+    private SpriteRenderer spriteRenderer;
+    private BoxCollider2D boxCollider2D;
 
     private void Start()
     {
         audioSource = GetComponent<AudioSource>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        boxCollider2D = GetComponent<BoxCollider2D>();
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -16,7 +20,10 @@ public class Collectable : MonoBehaviour {
         if (other.CompareTag("Player"))
         {
             audioSource.Play();
-            Destroy(gameObject);
+            spriteRenderer.enabled = false;
+            boxCollider2D.enabled = false;
+            float delay = audioSource.clip.length;
+            Destroy(gameObject, delay);
         }
     }
 }
